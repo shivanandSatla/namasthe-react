f1ec50e37424d168490c1aebfea53194fe115635
unknown <shivanandsrinivas1995@gmail.com>
2024-03-29 16:15:04 +0530

    REACT-01: Basic react

---
+++ App.js
@@ -0,0 +1,18 @@
+const parent = React.createElement('div', {
+    id: 'parent'
+}, 
+    [
+        React.createElement("div", {id: "child1"}, [
+            React.createElement("h1", {}, "I am h1 tag"),
+            React.createElement("h1", {}, "I am h2 tag")
+        ]),
+        React.createElement("div", {id: "child2"}, [
+            React.createElement("h1", {}, "I am h1 tag"),
+            React.createElement("h1", {}, "I am h2 tag")
+        ])
+    ]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+setTimeout(() => {
+    root.render(parent);
+}, 1000)
